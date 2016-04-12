593ba9434c79899252b0f3ef18561ec3ad9ee6c7
Sagi Greenstine <sagigr@gmail.com>
2016-04-13 00:13:56 +0300

    Update server.R

--- server.R
+++ server.R
@@ -1,39 +1,52 @@
 library(shiny)
-library(dplyr)
-library(ggplot2movies)
-# Shiny server
+
+source("./model.R", local = TRUE)
+
 shinyServer(
-  function(input, output) {
-    
-    filterByInputCriteria <- function(dt, 	minYear, maxYear, 
-                                      minRating, maxRating, 
-                                      minBudget, maxBudget, 
-                                      minVote, maxVote, 
-                                      mpaas, genres) {
-      
-      result <- dt %>% filter(year >= minYear 
-                              ,year <= maxYear
-                              ,rating >= minRating, rating <= maxRating
-                              ,votes >= minVote, votes <= maxVote
-                              ,mpaa %in% mpaas
-      )
-      for (g in genres) {
-        result <- result %>% filter(result[,g] == 1)
-      }
-      
-      return(result)
-    }
-    
-    # Prepare dataset
-    dataTable <- reactive({
-      filterByInputCriteria(movies, input$years[1], 
-                            input$years[2], input$rating[1],
-                            input$rating[2], input$budget[1],
-                            input$budget[2], input$votes[1],
-                            input$votes[2], input$mpaa, input$genres)
-    })
-    
-    # Render movies table
-    output$dTable <- renderDataTable(dataTable(), options = list(columnDefs = list(list(visible=FALSE, targets=c(6:15,17:23)))))    
-    # end of function(input, output)
-  })
+    function(input, output, session){
+        
+        
+        prediction <- reactive({
+            nextWordPredictor(input$inputTxt)
+        })
+        
+        output$words <- renderUI( {
+            predictWords <- prediction()
+            assign('savedWords', predictWords, envir=.GlobalEnv)
+            n <- length(predictWords)
+            if( n > 0 && nchar(predictWords) > 0) {
+                buttons <- list()
+                for(i in 1:n) {
+                    buttons <- list(buttons, list(
+                        actionButton(inputId = paste("word",i, sep = ""), label =predictWords[i])
+                    ))
+                }
+               
+                tagList(
+                   buttons 
+                )
+            } else {
+                tagList("") 
+            }
+        })
+        
+        observeEvent(input$word1, {
+            updateTextInput(session, "inputTxt", value = paste(input$inputTxt, get('savedWords', envir=.GlobalEnv)[1]))
+        })
+        
+        observeEvent(input$word2, {
+            updateTextInput(session, "inputTxt", value = paste(input$inputTxt, get('savedWords', envir=.GlobalEnv)[2]))
+        })
+        
+        observeEvent(input$word3, {
+            updateTextInput(session, "inputTxt", value = paste(input$inputTxt, get('savedWords', envir=.GlobalEnv)[3]))
+        })
+        
+        observeEvent(input$word4, {
+            updateTextInput(session, "inputTxt", value = paste(input$inputTxt, get('savedWords', envir=.GlobalEnv)[4]))
+        })
+        
+        observeEvent(input$reset, {
+            updateTextInput(session, "inputTxt", value = paste(NULL))
+        })
+})
